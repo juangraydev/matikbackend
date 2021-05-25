6c732a80fab05586b511281386108660e7927640
juangraydev <jwsabala@gmail.com>
2021-05-25 18:17:57 +0800

    add

--- routes/auth.js
+++ routes/auth.js
@@ -7,14 +7,31 @@ const db = require('../config/database');
 router
     .route("/login")
     .post(async (req, res) => {
-        console.log(req.params);
+        console.log(req.body);
+        
         res.sendStatus(200);
+        // try {
+        //     const users = await usersController.findByEmail(req.body.email);
+        //     bcrypt.compare(req.body.password, users.password, function(err, result) {
+        //         // result == true
+        //         console.log(result);
+        //         if (result){
+        //             let userToken = genToken(users.id);
+        //             res.status(200).json({token: userToken, displayname: users.name, email: users.email});
+        //         }else {
+        //             res.status(401).send();
+        //         }
+        //     });
+        // } catch(e) {
+        //     console.log(e);
+        // }
     })
 
 router
     .route("/register")
     .post( async(req, res) => {
         const {name, email, password} = req.body;
+        console.log(req.body);
 
         try {
             const hashedPassword = await new Promise((resolve, reject) => {
@@ -26,8 +43,6 @@ router
                     }
                 });
             })
-
-            console.log(hashedPassword);
 
             let user = await db.query(insertUsers({name, email, hashedPassword}));
             console.log(user);
--- server.js
+++ server.js
@@ -4,9 +4,17 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 8000;
-var cors = require('cors')
+var cors = require('cors');
+// Add a list of allowed origins.
+// If you have more origins you would like to add, you can add them to the array below.
+const allowedOrigins = ['http://localhost:5000'];
 
-app.use(cors())
+const options = {
+  origin: allowedOrigins,
+  credentials: true
+};
+
+app.use(cors(options))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
@@ -14,7 +22,6 @@ app.use(bodyParser.urlencoded({extended: false}))
 const auth = require('./routes/auth');
 
 app.use('/api/v1/auth', auth);
-
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
